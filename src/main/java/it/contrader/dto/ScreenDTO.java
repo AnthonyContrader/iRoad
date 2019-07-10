93280820ba6175d346f3ced494e881af1265982c
DeVecchis <devecchisriccardo@live.it>
2019-07-10 12:32:18 +0200

    backend screen entity

---
+++ src/main/java/it/contrader/dto/ScreenDTO.java
@@ -0,0 +1,16 @@
+package it.contrader.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ScreenDTO {
+
+	private Integer idScreen;
+
+	private String output;
+	
+}
